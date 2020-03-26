b30a8748d85d9450c75e5381cf909cf5180cc56c
Lucien Mary <marylucien1998@gmail.com>
2020-03-26 21:09:38 +0100

    css, route vers les diffs pages

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 // Imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var apiRouter = require('./apiRouter').router;
 
 // Instantiate server
 var server = express();
 
+// distribution des fichiers statics.
+server.use(express.static('public'));
+
 // Body Parser config.
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 
-// Configure routes.
+
+
+
+// Configuration des routes.
 server.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Hello!</h1>');
-});
+    res.status(200).sendFile(path.join(__dirname + '/public/'));
+})
+.get('/contact', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).sendFile(path.join(__dirname + '/public/contact.html'));
+})
+.get('/regles', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).sendFile(path.join(__dirname + '/public/tuto.html'));
+})
+.get('/vie-privee', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).sendFile(path.join(__dirname + '/public/vie-privee.html'));
+})
+.get('/register', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).sendFile(path.join(__dirname + '/public/register.html'));
+})
+.get('/login', function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).sendFile(path.join(__dirname + '/public/login.html'));
+})
+
+
 
 server.use('/api/', apiRouter);
 
 // Launch server.
 server.listen(8080, function(){
     console.log('Serveur en écoute 🔥');
-    console.log('fr');
 })
